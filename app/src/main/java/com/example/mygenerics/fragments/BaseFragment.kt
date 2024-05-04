9fb9451a50ed16a7db2c6796d06e1dc01f29d1b3
TeymurMemmedov <teymurmemmedov314@gmail.com>
2024-05-04 17:10:56 +0400

    --no-edit

--- app/src/main/java/com/example/mygenerics/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/mygenerics/fragments/BaseFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
+// Source : https://stackoverflow.com/a/73517663
 abstract class BaseFragment<T : ViewBinding>(
     private val viewBindingInflater: (
         inflater: LayoutInflater,
